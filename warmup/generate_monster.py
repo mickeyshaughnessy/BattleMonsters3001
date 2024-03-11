23b6eb24cb340ff0ee0ac7abbe7b5ce8803b3ff9
Michael Shaughnessy <mickeyshaughnessy>
2024-03-10 22:13:20 -0600

    update monsters

--- warmup/generate_monster.py
+++ warmup/generate_monster.py
@@ -1,17 +1,36 @@
-from prompts import generate_monster
+from prompts import generate_monster, make_seeds
 
 import requests
 import json
 
-resp = requests.post(
-        "http://localhost:11434/api/generate", 
-        json={"model" : "mistral", "prompt" : generate_monster % input()},
-        stream=False)
+def execute_completion(prompt):
 
-_text = ""
-for r in resp.iter_lines():
-    r = json.loads(r)
-    _text += r.get("response")
+    resp = requests.post(
+            "http://localhost:11434/api/generate", 
+            json={"model" : "mistral", "prompt" : prompt},
+            stream=False)
+    
+    _text = ""
+    for r in resp.iter_lines():
+        r = json.loads(r)
+        _text += r.get("response")
+    return _text
 
-print(_text)
-print(json.loads(_text))
+
+if __name__ == "__main__":
+    _text = execute_completion(make_seeds % 16)
+    with open("generated_seeds.txt", 'w') as f:
+        f.write(_text)
+
+    
+    with open("generated_monsters.txt", 'w') as f:
+        lines = _text.split("\n")
+        for line in lines:
+            monster = execute_completion(generate_monster % line)
+            monster.replace("\n", "")
+            print(monster)
+            try:
+                f.write(monster+"\n")
+                json.loads(monster)
+            except:
+                print("failed json loads")
--- warmup/prompts.py
+++ warmup/prompts.py
@@ -9,6 +9,8 @@ generate_monster = """
     "speed" : "29",
     "energy" : 10,
     "monster_types" : "Fire Ape Monk",
+    "weakness" : "All forms of Water",
+    "immunity" : "Mind Control and Temptation",
     "abilities" : "Fire Eye - range distance attack enabled",
     "seed" : "Fire Monkey Librarian"
     }
@@ -16,4 +18,23 @@ generate_monster = """
     Input_prompt = %s
     
     Output = 
+    """
+
+make_seeds = """
+    Generate %s MonsterBattle 3001 monster seed phrases.
+
+    1. red fox hunter
+    2. Giga Ape druid
+    3. Elemental octopus ninja
+    4. Godzilla Uberdinosaur Spirit
+    5. Undead zombie warlock
+    6. Blue Dragon Lumberjack
+    7. Dark Elf Detective
+    8. Alien Cyborg Warrior
+    9. Zombie Insect
+    10. Giant Elk Priestess
+    11. Elder Dragon Nicol Bolas
+    12. Elemental Lightning Crew
+    13. Apprentice Necromancer
+    14. 
     """

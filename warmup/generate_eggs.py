bb9fb82aae2d9d253af5817b6c7bff96d9942077
Michael Shaughnessy <mickeyshaughnessy>
2024-03-12 15:35:08 -0600

    add eggs file

---
+++ warmup/generate_eggs.py
@@ -0,0 +1,12 @@
+
+from prompts import generate_eggs
+from prompts import hatch_egg
+
+def generate_eggs():
+    # Create N new egg tokens from Monster parents (Mx, My).
+
+def hatch_egg(Egg):
+    # Given an egg token as input, hatch the egg and create the Monster token.
+    execute_completion
+if __name__ == "__main__":
+

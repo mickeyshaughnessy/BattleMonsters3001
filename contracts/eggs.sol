72d7a64ca82f77fd97467a082f24db70788f072f
Michael Shaughnessy <mickeyshaughnessy>
2024-03-16 15:33:40 -0600

    tidy up Egg contract

--- contracts/eggs.sol
+++ contracts/eggs.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IMonsters {
     function mint(address to, string memory uri) external;
@@ -13,22 +13,9 @@ interface IMonsters {
 contract Eggs is ERC721URIStorage, Ownable, ReentrancyGuard {
     uint public nextTokenId;
     address public monstersAddress;
-    address public monsterBucksAddress;
-
-    struct Order {
-        bool isBuyOrder;
-        uint tokenId;
-        uint monsterBucksAmount;
-        address maker;
-    }
-
-    mapping(uint => Order) public orders;
-    uint public nextOrderId;
 
     event Minted(address to, uint tokenId, string uri);
     event Hatched(uint tokenId, address owner, string newMonsterURI);
-    event OrderPlaced(uint orderId, bool isBuyOrder, uint tokenId, uint monsterBucksAmount, address maker);
-    event OrderCancelled(uint orderId);
 
     constructor() ERC721("Eggs", "EGG") {}
 
@@ -45,26 +32,5 @@ contract Eggs is ERC721URIStorage, Ownable, ReentrancyGuard {
         _burn(tokenId);
         IMonsters(monstersAddress).mint(owner, newMonsterURI);
         emit Hatched(tokenId, owner, newMonsterURI);
-    }
-
-    function placeLimitOrder(bool isBuyOrder, uint tokenId, uint monsterBucksAmount) public nonReentrant {
-        require(_exists(tokenId), "Egg does not exist.");
-        if (isBuyOrder) {
-            require(IERC20(monsterBucksAddress).transferFrom(msg.sender, address(this), monsterBucksAmount), "MonsterBucks transfer failed.");
-        } else {
-            require(ownerOf(tokenId) == msg.sender, "You are not the owner of this Egg.");
-        }
-        orders[nextOrderId] = Order(isBuyOrder, tokenId, monsterBucksAmount, msg.sender);
-        emit OrderPlaced(nextOrderId, isBuyOrder, tokenId, monsterBucksAmount, msg.sender);
-        nextOrderId++;
-    }
-
-    function cancelOrder(uint orderId) public nonReentrant {
-        require(orders[orderId].maker == msg.sender, "You are not the maker of this order.");
-        if (orders[orderId].isBuyOrder) {
-            IERC20(monsterBucksAddress).transfer(msg.sender, orders[orderId].monsterBucksAmount);
-        }
-        delete orders[orderId];
-        emit OrderCancelled(orderId);
     }
 }
